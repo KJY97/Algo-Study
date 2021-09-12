cfc51d8fed3a8f05c5b8df327959a4f1036d84f0
KJY97 <47266337+KJY97@users.noreply.github.com>
2021-09-13 02:28:21 +0900

    Update BOJ_1927.java

--- 2week/KJY97/BOJ_1927.java
+++ 2week/KJY97/BOJ_1927.java
@@ -1,4 +1,4 @@
-//실행시간: 292 ms
+//실행시간: 284 ms
 //메모리: 25956 kb
 
 import java.io.BufferedReader;
@@ -14,21 +14,18 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());	//연산의 개수
 		
 		//우선순위 = 작은 순자 순
-		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		
 		//자연수 - 추가, 0 - 가장 작은 값 출력
 		for (int i = 0; i < N; i++) {
 			int X = Integer.parseInt(br.readLine());
 			
-			if(X > 0) priorityQueue.offer(X);
+			if(X > 0) pq.offer(X);
 			else {
-				//int형으로 null 체크 불가.
-				Integer num = priorityQueue.poll();
-				
-				//null은 객체가 아니여서 equals로 비교 불가
-				if(num == null) num = 0;
-
-				sb.append(num).append('\n');
+				if(pq.isEmpty()) 
+					sb.append(0).append('\n');
+				else
+					sb.append(pq.poll()).append('\n');
 			}
 		}
 		System.out.println(sb);
