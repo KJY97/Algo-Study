8e15bb508f3912bd468d7ad21ec4edfdba27b39a
yunwonjeong <90126101+yunwonjeong@users.noreply.github.com>
2021-12-14 09:48:02 +0900

    Update BOJ_2096.java

--- 14week/yunwonjeong/BOJ_2096.java
+++ 14week/yunwonjeong/BOJ_2096.java
@@ -1,30 +1,58 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class boj2442 {
+public class boj2096 {
 
-   public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int N = Integer.parseInt(br.readLine().trim());
-      for (int i = 1; i <= N; ++i) {
-         for (int j = N - i; j > 0; --j) {
-            System.out.print(" ");
-         }
-         for (int k = 1; k <= (2 * i) - 1; ++k) {
-            System.out.print("*");
-         }
-         System.out.println();
-      }
-      for (int i = 1; i < N; ++i) {
-         for (int j = 0; j < i; ++j) {
-            System.out.print(" ");
-         }
-         for (int k = 1; k <= ((2 * N) - (2 * (i+1)) + 1); ++k) {
-            System.out.print("*");
-         }
-         
-            System.out.println();
-            
-      }
-   }
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		
+		int[][] min = new int[N+1][3];
+		int[][] max = new int[N+1][3];
+		int[][] arr = new int[N+1][3];
+		
+		min[0][0] = min[0][1] = min[0][2] = 0;
+		max[0][0] = max[0][1] =	max[0][2] = 0;
+		
+		for(int i = 1 ;i <= N; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+			arr[i][2] = Integer.parseInt(st.nextToken());
+
+			
+		}
+		
+		
+		for(int i = 1; i <=N ; i++) {
+			
+			
+			min[i][0] = Math.min(min[i-1][0]+arr[i][0], min[i-1][1]+arr[i][0]);
+			max[i][0] = Math.max(max[i-1][0]+arr[i][0], max[i-1][1]+arr[i][0]);
+			
+			min[i][1] = Math.min(min[i-1][0]+arr[i][1], min[i-1][1]+arr[i][1]);
+			min[i][1] = Math.min(min[i][1], min[i-1][2]+arr[i][1]);
+			max[i][1] = Math.max(max[i-1][0]+arr[i][1], max[i-1][1]+arr[i][1]);
+			max[i][1] = Math.max(max[i][1], max[i-1][2]+arr[i][1]);
+			
+			min[i][2] = Math.min(min[i-1][1]+arr[i][2], min[i-1][2]+arr[i][2]);
+			max[i][2] = Math.max(max[i-1][1]+arr[i][2], max[i-1][2]+arr[i][2]);
+			
+		
+		}
+		
+		
+		int a = Math.min(min[N][0], min[N][1]);
+		a = Math.min(a, min[N][2]);
+		int b = Math.max(max[N][0], max[N][1]);
+		b = Math.max(b, max[N][2]);
+		
+		System.out.println(b+" "+a);
+	}
+
 }
